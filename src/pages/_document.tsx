3c0683840c3c92e23e54cf00058cdac48473ddf4
Alan Almeida <alanalmeida.emp@gmail.com>
2021-06-15 10:16:55 -0300

    public folder moved out of src

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,10 @@
 import Document, { Html, Head, Main, NextScript} from 'next/document'
 
-export class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <title>AdoPet</title>
-
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
         </Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Flex, Image } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <div>
-      <h1>AdoPet</h1>
-    </div>
+    <Flex
+      w="100vw"
+      h="100vh"
+      bg="purple.500"
+    >
+      <Image
+        boxSize="fit-content"
+        objectFit="cover"
+        src="url(src/public/logo.svg)"
+        alt="Logo"
+      />
+    </Flex>
   )
 }
