6ce65b615ace66ba10e85405262dafb7f7e56fc6
Anderson Campos <anderson.campos@pipefy.com>
2021-06-16 09:11:57 -0300

    Header component

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,37 +1,44 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
-  colors: {
-    gray: {
-      "50": "#FAFAFA"
-    },
-    green: {
-      "300": "#14D361"
-    },
-    purple: {
-      "200": "#9871F5",
-      "500": "#8257E5",
-    }
-  },
-  textStyles: {
-    h1: {
-      fontsize: ["2.5rem", "8xl"],
-      fontWeight: "bold",
-      color: "gray.50",
-    },
-    h2: {
-      fontSize: ["xl", "6xl"],
-      fontWeight: "bold",
-      color: "#FFFF",
-    },
-    p: {
-      fontSize: ["lg", "4xl"],
-      fontWeight: "normal",
-      color: "gray.50",
-    }
-  },
-  fonts: {
-    heading: 'Roboto',
-    body: 'Roboto'
-  }
-})
+	colors: {
+		gray: {
+			'50': '#FAFAFA',
+		},
+		green: {
+			'300': '#14D361',
+		},
+		purple: {
+			'200': '#9871F5',
+			'500': '#8257E5',
+		},
+	},
+	textStyles: {
+		h1: {
+			fontsize: ['2.5rem', '8xl'],
+			fontWeight: 'bold',
+			color: 'gray.50',
+		},
+		h2: {
+			fontSize: ['xl', '6xl'],
+			fontWeight: 'bold',
+			color: '#FFFF',
+		},
+		p: {
+			fontSize: ['lg', '4xl'],
+			fontWeight: 'normal',
+			color: 'gray.50',
+		},
+	},
+	fonts: {
+		heading: 'Roboto',
+		body: 'Roboto',
+	},
+	styles: {
+		global: {
+			body: {
+				background: 'gray.100',
+			},
+		},
+	},
+});
