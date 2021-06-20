293a513973aae8477e67735fda358efdf2cef845
Alan Almeida <alanalmeida.emp@gmail.com>
2021-06-20 00:39:18 -0300

    installed react-query | created queryClient.ts | add QueryClientProvider in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { AuthProvider } from '../contexts/AuthContext'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '../services/queryClient'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
